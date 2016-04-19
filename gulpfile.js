1377370d12da0be6ebbb3b9e375a290ece377d10
deepak6sp <deepak6sp@gmail.com>
2016-04-19 21:29:04 +1000

    gulp configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,56 @@
 var gulp = require("gulp");
 var browserSync = require('browser-sync').create();
+var sass= require("gulp-sass");
+var mincss = require("gulp-cssmin");
+var rename = require('gulp-rename');
 
 var PATHS = {
-	html : '*.html',
-	js : './js/*.js',
-	css : './css/*/.css'
+    "source":{
+    	html : '*.html',
+    	js : './js/*.js',
+    	css : './css/*/.css',
+        scss : './scss/*.scss'
+    },
+    "destination":{
+        css : "./css/",
+        js : "./js/"
+    },
+    "bootstrap":{
+        css : './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        js : './node_modules/bootstrap/dist/js/bootstrap.min.js'
+    }
 
 };
 
-gulp.task('serve', function () {
+gulp.task('copy-css',function(){
+    return gulp.src(PATHS.bootstrap.css)
+    .pipe(gulp.dest(PATHS.destination.css));
+
+});
+
+gulp.task('copy-js',function(){
+    return gulp.src(PATHS.bootstrap.js)
+    .pipe(gulp.dest(PATHS.destination.js));
+
+});
+
+gulp.task('sassify',['watch'],function(){
+    return gulp.src(PATHS.source.scss)
+        .pipe(sass())
+        .pipe(mincss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(PATHS.destination.css))
+        .pipe(browserSync.stream());
+
+});
+
+gulp.task('watch',function(){
+    gulp.watch(PATHS.source.html).on("change", browserSync.reload);
+    gulp.watch(PATHS.source.scss,['sassify']);
+    gulp.watch(PATHS.source.js).on("change", browserSync.reload);
+});
+
+gulp.task('serve', ['copy-css','copy-js','sassify','watch'], function () {
 
     // Serve files from the root of this project
     browserSync.init({
@@ -17,7 +59,7 @@ gulp.task('serve', function () {
         }
     });
 
-    gulp.watch(PATHS.html).on("change", browserSync.reload);
+    
 });
 
 gulp.task("default",['serve'],function(){
