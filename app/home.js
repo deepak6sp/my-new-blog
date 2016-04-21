bde3f32351bdea2a97e8d863b844031a1dbb573c
deepak6sp <deepak6sp@gmail.com>
2016-04-21 21:12:35 +1000

    navbar added

--- app/home.js
+++ app/home.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-	var window_height=$(window).height();
-    $("#main-background-image").height(window_height);
+    $(this).scrollTop(0);
+    var window_height=$(window).height();
     $(".blog-post-icon").click(function(){
-
+    	$('html, body').animate({scrollTop: window_height+2}, 1000);
     });
-}); 
+    window_resize();
+    $(window).resize(function(){
+    	window_resize();
+    });
+   
+});  
+
+
+function window_resize(){
+	var window_height=$(window).height();
+	$("#main-background-image").height(window_height);
+}
