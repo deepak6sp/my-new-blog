a674a455e369403c3d993a305be57cf097d1470d
deepak6sp <deepak6sp@gmail.com>
2016-04-26 20:51:53 +1000

    react components created

--- app/components/backgroundPage.js
+++ app/components/backgroundPage.js
@@ -4,6 +4,19 @@ class BackgroundPage extends React.Component {
     constructor(props) {
         super(props);
     }
+    componentDidMount(){
+        $(this).scrollTop(0);
+        var window_height=$(window).height();
+        $(".blog-post-icon").click(function(){
+            $('html, body').animate({scrollTop: window_height+2}, 1000);
+        });
+        $("#main-background-image").height(window_height);
+        $(".main-background-overlay").height(window_height);
+        $(window).resize(function(){
+            $("#main-background-image").height(window_height);
+            $(".main-background-overlay").height(window_height);
+        }); 
+    }
     render() {
         return  (
         	<header className="home-page-header">
