1343fe7254a2779e8afc15696b9e05cee4278346
deepak6sp <deepak6sp@gmail.com>
2016-05-18 20:42:43 +1000

    social links added

--- app/components/shared_components/socialLinks.js
+++ app/components/shared_components/socialLinks.js
@@ -1,18 +1,39 @@
 import React,{ Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 
+const socialLinks=[{
+                    "name":"twitter",
+                    "url":"https://twitter.com/deepak6sp"
+                },
+                {
+                    "name":"github",
+                    "url":"https://github.com/deepak6sp"
+                },
+                {
+                    "name":"stackoverflow",
+                    "url":"http://stackoverflow.com/users/3713726/d-pac"
+                },
+                {
+                    "name":"linkedIn",
+                    "url":"https://au.linkedin.com/in/deepak-prakash-a1bb2825"
+                }];
 
 class SocialLinks extends Component {
     constructor(props) {
         super(props);
-        const socialLinks=["github","stackoverflow","twitter"];
+        this.state = {
+            socialLinks: socialLinks
+        };
     }
     render() {
+        let SLinks = this.state.socialLinks.map(function(slink,index){
+                return (
+                    <a target="_blank" key={index} className={slink.name} href={slink.url}>{slink.name}</a>
+                );
+        });
         return  (
 			<div className="social-links">
-				<span className="github">github</span>
-				<span className="stackoverflow">stackoverflow</span>
-				<span className="twitter">twitter</span>
+                {SLinks}
 			</div>
         );
     }
