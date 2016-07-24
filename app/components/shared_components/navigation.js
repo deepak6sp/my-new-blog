0addf33279e9e25965b45038318fcff80dd4a4f9
Deepak Prakash <deepak@Deepaks-MacBook-Pro.local>
2016-07-24 14:11:53 +1000

    navaar changes

--- app/components/shared_components/navigation.js
+++ app/components/shared_components/navigation.js
@@ -6,41 +6,51 @@ import Scroll from 'react-scroll';
 
 var ScrollLink = Scroll.Link;
 
-const Navigation = () => {
-        return  (
-        	<Navbar fixedTop>
-                {/* Brand and toggle get grouped for better mobile display */}
-                <Navbar.Header>
-                  <Navbar.Brand>
-                     <Link to="home">Deepak Prakash</Link>
-                  </Navbar.Brand>
-                  <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse> 
+class Navigation extends Component {
+    constructor(props){
+      super(props);
+    }
+    _handleClose(){
+      //document.querySelector("#menuClose").style.display = "none";
+      //document.getElementById("navbar").classList.remove("navbar-collapse-secondary");
+    }
+    render(){
+      return  (
+      	<Navbar fixedTop id="navbar">
+              {/* Brand and toggle get grouped for better mobile display */}
+              <Navbar.Header>
+                <Navbar.Brand>
+                   <Link to="home">Deepak Prakash</Link>
+                </Navbar.Brand>
+                <FontAwesome className='menuClose' name='close' size='2x' onClick={this._handleClose}/>
+                <Navbar.Toggle />
+              </Navbar.Header>
+              <Navbar.Collapse> 
 
-                  {/* Collect the nav links, forms, and other content for toggling */}
-                  <Nav pullRight>
-                    
-                     <li>
-                      <Link to="skills">SKILLS</Link>
-                    </li>
+              {/* Collect the nav links, forms, and other content for toggling */}
+              <Nav pullRight>
+                
+                 <li>
+                  <Link to="skills">SKILLS</Link>
+                </li>
 
-                     <li>
-                      <Link to="projects">PROJECTS</Link>
-                    </li>
+                 <li>
+                  <Link to="projects">PROJECTS</Link>
+                </li>
 
-                    <li>
-                      <Link to="blog">BLOG</Link>
-                    </li>
+                <li>
+                  <Link to="blog">BLOG</Link>
+                </li>
 
-                    <li>
-                      <Link to="contact">CONTACT</Link>
-                    </li>
-                  
-                  </Nav>
-              </Navbar.Collapse>
-        	</Navbar>
-        );
+                <li>
+                  <Link to="contact">CONTACT</Link>
+                </li>
+              
+              </Nav>
+            </Navbar.Collapse>
+      	</Navbar>
+      );
+    }
 }
 
 export default Navigation;
--- public/minifyjs/bundle.js
+++ public/minifyjs/bundle.js
@@ -28387,6 +28387,8 @@
 	  value: true
 	});
 	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
 	var _react = __webpack_require__(1);
 	
 	var _react2 = _interopRequireDefault(_react);
@@ -28405,72 +28407,100 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
 	var ScrollLink = _reactScroll2.default.Link;
 	
-	var Navigation = function Navigation() {
-	  return _react2.default.createElement(
-	    _reactBootstrap.Navbar,
-	    { fixedTop: true },
-	    _react2.default.createElement(
-	      _reactBootstrap.Navbar.Header,
-	      null,
-	      _react2.default.createElement(
-	        _reactBootstrap.Navbar.Brand,
-	        null,
+	var Navigation = function (_Component) {
+	  _inherits(Navigation, _Component);
+	
+	  function Navigation(props) {
+	    _classCallCheck(this, Navigation);
+	
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Navigation).call(this, props));
+	  }
+	
+	  _createClass(Navigation, [{
+	    key: '_handleClose',
+	    value: function _handleClose() {
+	      //document.querySelector("#menuClose").style.display = "none";
+	      //document.getElementById("navbar").classList.remove("navbar-collapse-secondary");
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        _reactBootstrap.Navbar,
+	        { fixedTop: true, id: 'navbar' },
 	        _react2.default.createElement(
-	          _reactRouter.Link,
-	          { to: 'home' },
-	          'Deepak Prakash'
+	          _reactBootstrap.Navbar.Header,
+	          null,
+	          _react2.default.createElement(
+	            _reactBootstrap.Navbar.Brand,
+	            null,
+	            _react2.default.createElement(
+	              _reactRouter.Link,
+	              { to: 'home' },
+	              'Deepak Prakash'
+	            )
+	          ),
+	          _react2.default.createElement(_reactFontawesome2.default, { className: 'menuClose', name: 'close', size: '2x', onClick: this._handleClose }),
+	          _react2.default.createElement(_reactBootstrap.Navbar.Toggle, null)
+	        ),
+	        _react2.default.createElement(
+	          _reactBootstrap.Navbar.Collapse,
+	          null,
+	          _react2.default.createElement(
+	            _reactBootstrap.Nav,
+	            { pullRight: true },
+	            _react2.default.createElement(
+	              'li',
+	              null,
+	              _react2.default.createElement(
+	                _reactRouter.Link,
+	                { to: 'skills' },
+	                'SKILLS'
+	              )
+	            ),
+	            _react2.default.createElement(
+	              'li',
+	              null,
+	              _react2.default.createElement(
+	                _reactRouter.Link,
+	                { to: 'projects' },
+	                'PROJECTS'
+	              )
+	            ),
+	            _react2.default.createElement(
+	              'li',
+	              null,
+	              _react2.default.createElement(
+	                _reactRouter.Link,
+	                { to: 'blog' },
+	                'BLOG'
+	              )
+	            ),
+	            _react2.default.createElement(
+	              'li',
+	              null,
+	              _react2.default.createElement(
+	                _reactRouter.Link,
+	                { to: 'contact' },
+	                'CONTACT'
+	              )
+	            )
+	          )
 	        )
-	      ),
-	      _react2.default.createElement(_reactBootstrap.Navbar.Toggle, null)
-	    ),
-	    _react2.default.createElement(
-	      _reactBootstrap.Navbar.Collapse,
-	      null,
-	      _react2.default.createElement(
-	        _reactBootstrap.Nav,
-	        { pullRight: true },
-	        _react2.default.createElement(
-	          'li',
-	          null,
-	          _react2.default.createElement(
-	            _reactRouter.Link,
-	            { to: 'skills' },
-	            'SKILLS'
-	          )
-	        ),
-	        _react2.default.createElement(
-	          'li',
-	          null,
-	          _react2.default.createElement(
-	            _reactRouter.Link,
-	            { to: 'projects' },
-	            'PROJECTS'
-	          )
-	        ),
-	        _react2.default.createElement(
-	          'li',
-	          null,
-	          _react2.default.createElement(
-	            _reactRouter.Link,
-	            { to: 'blog' },
-	            'BLOG'
-	          )
-	        ),
-	        _react2.default.createElement(
-	          'li',
-	          null,
-	          _react2.default.createElement(
-	            _reactRouter.Link,
-	            { to: 'contact' },
-	            'CONTACT'
-	          )
-	        )
-	      )
-	    )
-	  );
-	};
+	      );
+	    }
+	  }]);
+	
+	  return Navigation;
+	}(_react.Component);
 	
 	exports.default = Navigation;
 
